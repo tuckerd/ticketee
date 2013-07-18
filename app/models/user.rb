d9cc5073271c62ed5b754cd0a7ed1a5bb01ddd2a
D Tucker <danielle@tuckerlabs.com>
2013-07-17 23:08:00 -0700

    Add the ability to create admin users through the admin backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, 
           :password_confirmation, :remember_me  
 
+  attr_accessible :email, :password, :admin, as: :admin
+
   # attr_accessible :title, :body
 
-
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User"})"
+  end
 end
